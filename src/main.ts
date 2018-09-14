37d431647dda9b363fc8caa19679646dad4ab020
Omkar Yadav <httpsomkar@gmail.com>
2018-09-14 23:23:27 +0530

    Added code coverage

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import config from './config';
 
-// @ts-ignore
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(config.get('port'));
